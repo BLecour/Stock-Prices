299d6e4c90fcb831f8eeb0cc588d94f04fea766c
Ben <63076037+bentheball1@users.noreply.github.com>
2024-05-27 18:10:59 -0400

    fixed !split if there were multiple splits

--- Stock Prices Bot.py
+++ Stock Prices Bot.py
@@ -32,7 +32,7 @@ async def on_message(message):
             price = stock.info["currentPrice"]
         except (IndexError, KeyError):
             # give error if the stock doesn't exist
-            await message.channel.send(":no_entry_sign: An error has occured. Check the spelling of the stock ticker and try again.")
+            await message.channel.send(":no_entry_sign: An error has occured. This stock ticker does not exist. Check the spelling of the stock ticker and try again.")
 
         # always have 2 decimals in price, ex. $1.1 to $1.10
         if list(str(price)).count('.') == 0 or len(str(price).split('.', 1)[1]) < 2:
@@ -73,7 +73,7 @@ async def on_message(message):
         :money_with_wings: Get stock price: !get {stock symbol}\n"
         ":date: Get stock price on specific date: !get {stock symbol} {date: YYYY-MM-DD}\n"
         ":scissors: Get most recent stock split: !split {stock symbol}\n"
-        ":thinking: Get recommendation rating: !recommendation {stock symbol}\n"
+        ":thinking: Get recommendation rating: !rec {stock symbol}\n"
         ":chart_with_upwards_trend: Get stock price of today's biggest gainer: !topgainer\n"
         ":chart_with_downwards_trend: Get stock price of today's biggest loser: !toploser\n"
         ":bar_chart: Get stock's range for today: !range {stock symbol}")
@@ -87,29 +87,40 @@ async def on_message(message):
         if len(message.content.split(' ')) == 2:
             await printPrice(stockName)
 
+        # if the user specified a date, then execute this part
         else: 
-            # if the user specified a date, then execute this part
             # this will calculate the range
             date = str(message.content.split(' ')[2])
             dates = date.split('-')
 
             if len(dates) != 3:
                 await message.channel.send(":no_entry_sign: An error has occured. If you would like to get the stock price on a specific date, the syntax is: !get {stock symbol} {date: YYYY-MM-DD}")
+                return
             
+            # get the stock history info on that date
             endDate = int(dates[2]) + 1
             if endDate < 10:
                 endDates = dates[0] + '-' + dates[1] + '-' + '0' + str(endDate)
             else:
                 endDates = dates[0] + '-' + dates[1] + '-' + str(endDate)
             historyDF = stock.history(start=date, end=endDates, interval="1d", actions=False)
+
+            # check to see if data was returned for the specified date
             if historyDF.empty:
                 await message.channel.send(":no_entry_sign: An error has occured. Ensure that the selected date is a business day and try again.")
+
             else:
                 historicalPrice = float(historyDF.iloc[0]["Close"])
-                if historicalPrice < 1: # if price is lower than $1, round to 4 decimals
+
+                # if price is lower than $1, round to 4 decimals
+                if historicalPrice < 1:
                     historicalPrice = format(historicalPrice, ".4f")
-                else: # if price is greater than or equal to 1, round to 2 decimals
+
+                # if price is greater than or equal to $1, round to 2 decimals
+                else:
                     historicalPrice = format(historicalPrice, ".2f")
+
+                # print the current price along with the historical price
                 currentPrice = format(stock.info["currentPrice"])
                 percentChange = round((float(currentPrice) - float(historicalPrice)) / float(historicalPrice) * 100, 2)
                 if float(currentPrice) >= float(historicalPrice):
@@ -118,25 +129,70 @@ async def on_message(message):
                     percentChange *= -1
                     await message.channel.send(f"The price of ${stockName.upper()} on {date} was ${historicalPrice}. Today, ${stockName.upper()} is worth ${currentPrice}, a {percentChange}% decrease :chart_with_downwards_trend:")
 
+    # get any stock split information
     if message.content.startswith("!split"):
+
+        # get the ticker info
         stockName = str(message.content.split(" ")[1])
         stock = yf.Ticker(stockName)
+        
+        # if the stock does not exist, give an error
+        try:
+            # try to access ticker info, it will throw an error if the stock does not exist
+            price = stock.info["currentPrice"]
+        except (IndexError, KeyError):
+            # give error if the stock doesn't exist
+            await message.channel.send(":no_entry_sign: An error has occured. This stock ticker does not exist. Check the spelling of the stock ticker and try again.")
+            return
+        
+        # get all info about splits from the ticker
         splitsDF = stock.splits
+
         if splitsDF.empty:
+            # give message if no splits have ever occured
             await message.channel.send(f":no_entry_sign: ${stockName.upper()} has never had a stock split.")
         else:
-            date = str(splitsDF.head().index[-1]).split(' ', 1)[0] # get date from pandas dataframe
-            split = float(splitsDF[-1])
-            if split < 1:
-                split = 1 / split
-                await message.channel.send(f"${stockName.upper()} had a 1 - {split} split on {date}.")
-            else:
-                await message.channel.send(f"${stockName.upper()} had a {split} - 1 split on {date}.")
 
-    if message.content.startswith("!recommendation"):
+            # change message depending on number of splits
+            if len(splitsDF == 1):          
+                messageString = f"${stockName.upper()} has had 1 stock split:\n"
+            else: 
+                messageString = f"${stockName.upper()} has had {len(splitsDF)} stock splits:\n"
+
+            # loop for each stock split that the stock has had
+            for i in range(len(splitsDF)):
+
+                # get date from pandas dataframe
+                date = str(splitsDF.index[i]).split(' ', 1)[0]
+
+                # change message depending on if it was a reverse split
+                split = float(splitsDF[i])
+                if split < 1:
+                    split = 1 / split
+                    messageString += f"{date}: 1 to {split}.\n"
+                else:
+                    messageString += f"{date}: {split} to 1.\n"
+
+            # send the entire message at once
+            await message.channel.send(messageString)
+
+    # get a stock's recommendation status
+    if message.content.startswith("!rec"):
+
+        # get the stock ticker and information
         stockName = str(message.content.split(" ")[1])
         stock = yf.Ticker(stockName)
-        recommendation = stock.info["recommendationKey"]
+
+        # get the current recommendation status for the ticker
+        try:
+            # try to access ticker info, it will throw an error if the stock does not exist
+            recommendation = stock.info["currentPrice"]
+        except (IndexError, KeyError):
+            # give error if the stock doesn't exist
+            await message.channel.send(":no_entry_sign: An error has occured. This stock ticker does not exist. Check the spelling of the stock ticker and try again.")
+            return
+
+        # print a different message depending on the recommendation status
         if recommendation == "strong buy":
             await message.channel.send(f":moneybag: ${stockName.upper()}'s recommendation rating is Strong Buy.")
         elif recommendation == "buy":
